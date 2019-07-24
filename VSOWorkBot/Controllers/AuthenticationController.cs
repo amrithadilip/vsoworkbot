69d9124c81489e2babe91443c7318d0376375fb8
Restyled.io <commits@restyled.io>
2019-07-24 22:13:00 +0000

    Restyled by astyle

--- VSOWorkBot/Controllers/AuthenticationController.cs
+++ VSOWorkBot/Controllers/AuthenticationController.cs
@@ -9,51 +9,51 @@ using VSOWorkBot.Models;
 
 namespace VSOWorkBot.Controllers
 {
-	[Route("")]
-	[AllowAnonymous]
-	public class AuthenticationController : Controller
-	{
-		private readonly AuthHelper authHelper;
+[Route("")]
+[AllowAnonymous]
+public class AuthenticationController : Controller
+{
+    private readonly AuthHelper authHelper;
 
-		private readonly string sessionKey = "UserData";
+    private readonly string sessionKey = "UserData";
 
-		public AuthenticationController(AuthHelper authHelper)
-		{
-			this.authHelper = authHelper;
-		}
+    public AuthenticationController(AuthHelper authHelper)
+    {
+        this.authHelper = authHelper;
+    }
 
-		[HttpGet]
-		[Route("signin/{conversationId}/{userId}")]
-		public IActionResult SignIn(string conversationId, string userId)
-		{
-			// Instruct the middleware corresponding to the requested external identity
-			// provider to redirect the user agent to its own authorization endpoint.
-			// Note: the authenticationScheme parameter must match the value configured in Startup.cs
-			if (String.IsNullOrEmpty(conversationId) || String.IsNullOrEmpty(userId))
-			{
-				return BadRequest(new ArgumentException("ConversationId or UserId cannot be empty"));
-			}
-			HttpContext.Session.SetString(sessionKey, JsonConvert.SerializeObject(new UserSession { ConversationId = conversationId, UserId = userId }));
-			return Redirect(authHelper.GetAuthorizeUrl());
-		}
+    [HttpGet]
+    [Route("signin/{conversationId}/{userId}")]
+    public IActionResult SignIn(string conversationId, string userId)
+    {
+        // Instruct the middleware corresponding to the requested external identity
+        // provider to redirect the user agent to its own authorization endpoint.
+        // Note: the authenticationScheme parameter must match the value configured in Startup.cs
+        if (String.IsNullOrEmpty(conversationId) || String.IsNullOrEmpty(userId))
+        {
+            return BadRequest(new ArgumentException("ConversationId or UserId cannot be empty"));
+        }
+        HttpContext.Session.SetString(sessionKey, JsonConvert.SerializeObject(new UserSession { ConversationId = conversationId, UserId = userId }));
+        return Redirect(authHelper.GetAuthorizeUrl());
+    }
 
-		[HttpGet]
-		[Route("oauth-callback")]
-		public async Task<IActionResult> GetAsync([FromQuery(Name = "code")] string code)
-		{
-			var sessionData = HttpContext.Session.GetString(sessionKey);
-			if (String.IsNullOrEmpty(sessionData) || String.IsNullOrEmpty(code))
-			{
-				return BadRequest(new ArgumentException("Invalid session. Please re-authenticate"));
-			}
-			var vsoToken = await authHelper.GetTokenAsync(code);
-			if (String.IsNullOrEmpty(vsoToken.AccessToken))
-			{
-				return BadRequest(new ArgumentException("Something went. Please re-authenticate"));
-			}
-			var userSession = JsonConvert.DeserializeObject<UserSession>(HttpContext.Session.GetString(sessionKey));
-			await authHelper.SaveTokenAsync(userSession.ConversationId, userSession.UserId, vsoToken.AccessToken);
-			return File("loggedin.html", "text/html");
-		}
-	}
+    [HttpGet]
+    [Route("oauth-callback")]
+    public async Task<IActionResult> GetAsync([FromQuery(Name = "code")] string code)
+    {
+        var sessionData = HttpContext.Session.GetString(sessionKey);
+        if (String.IsNullOrEmpty(sessionData) || String.IsNullOrEmpty(code))
+        {
+            return BadRequest(new ArgumentException("Invalid session. Please re-authenticate"));
+        }
+        var vsoToken = await authHelper.GetTokenAsync(code);
+        if (String.IsNullOrEmpty(vsoToken.AccessToken))
+        {
+            return BadRequest(new ArgumentException("Something went. Please re-authenticate"));
+        }
+        var userSession = JsonConvert.DeserializeObject<UserSession>(HttpContext.Session.GetString(sessionKey));
+        await authHelper.SaveTokenAsync(userSession.ConversationId, userSession.UserId, vsoToken.AccessToken);
+        return File("loggedin.html", "text/html");
+    }
+}
 }
--- VSOWorkBot/Models/VSOToken.cs
+++ VSOWorkBot/Models/VSOToken.cs
@@ -2,21 +2,36 @@ using Newtonsoft.Json;
 
 namespace VSOWorkBot.Models
 {
-	public class VSOToken
-	{
-		[JsonProperty("access_token")]
-		public string AccessToken { get; set; }
+public class VSOToken
+{
+    [JsonProperty("access_token")]
+    public string AccessToken {
+        get;
+        set;
+    }
 
-		[JsonProperty("refresh_token")]
-		public string RefreshToken { get; set; }
+    [JsonProperty("refresh_token")]
+    public string RefreshToken {
+        get;
+        set;
+    }
 
-		[JsonProperty("token_type")]
-		public string TokenType { get; set; }
+    [JsonProperty("token_type")]
+    public string TokenType {
+        get;
+        set;
+    }
 
-		[JsonProperty("expires_in")]
-		public int ExpiresIn { get; set; }
+    [JsonProperty("expires_in")]
+    public int ExpiresIn {
+        get;
+        set;
+    }
 
-		[JsonProperty("scope")]
-		public string Scope { get; set; }
-	}
+    [JsonProperty("scope")]
+    public string Scope {
+        get;
+        set;
+    }
+}
 }
